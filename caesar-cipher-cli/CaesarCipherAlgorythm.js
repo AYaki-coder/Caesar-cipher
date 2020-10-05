820cd8eb1c88267eaef43983e6985e5967fa9801
ayaki-coder <sj.a.yakimovich@gmail.com>
2020-10-05 21:41:38 +0300

    feat: add algorythm add options validations

---
+++ caesar-cipher-cli/CaesarCipherAlgorythm.js
@@ -0,0 +1,35 @@
+class CaesarCipher {
+  constructor(shift, isEncode) {
+    if (isEncode === 'decode') {
+      shift = 26 - shift;
+    }
+    this.shift = shift >= 0 ? (shift % 26) : 26 + (shift % 26);
+  }
+  
+  encode(inputString) {
+    console.log(this.shift);
+    const startABCUppecase = 'A'.charCodeAt(0);
+    const endABCUppecase = 'Z'.charCodeAt(0);
+    const startABC = 'a'.charCodeAt(0);
+    const endABC = 'z'.charCodeAt(0);
+    const newArr = [];
+
+    inputString.split('').forEach(el => {
+      if (el.charCodeAt(0) >= startABCUppecase && el.charCodeAt(0) <= endABCUppecase) {
+        const newCharCode = (el.charCodeAt(0) - startABCUppecase + this.shift) % 26 + startABCUppecase;
+        const newChar = String.fromCodePoint(newCharCode);
+        newArr.push(newChar);
+      } else if (el.charCodeAt(0) >= startABC && el.charCodeAt(0) <= endABC) {
+        const newCharCode = (el.charCodeAt(0) - startABC + this.shift) % 26 + startABC;
+        const newChar = String.fromCodePoint(newCharCode);
+        newArr.push(newChar);
+      } else {
+        newArr.push(el);
+      }
+    });
+
+    return (newArr.join(''));
+  }
+}
+
+module.exports = CaesarCipher;
--- caesar-cipher-cli/index.js
+++ caesar-cipher-cli/index.js
@@ -1,1 +1,23 @@
-console.log('start the task');
+const path = require('path');
+const fs = require('fs');
+const { program } = require('commander');
+const { pipeline } = require('stream');
+
+const CaesarCipher = require('./CaesarCipherAlgorythm');
+const validate = require('./validation');
+
+console.log(process.argv);
+
+program
+  .storeOptionsAsProperties(false)
+  .requiredOption('-s, --shift <shift>', 'a shift')
+  .requiredOption('-a, --action <action>', 'an action encode/decode')
+  .option('-i, --input <file>', 'an input file')
+  .option('-o, --output <file>', 'an output file');
+program.parse(process.argv);
+let { shift, action, input, output } = program.opts();
+console.log(input, output, shift, action);
+validate(input, output, shift, action);
+
+let obj = new CaesarCipher(7, false);
+console.log(obj.encode('Aopz pz zljyla. Tlzzhnl hivba "_" zftivs!'));
